3c5f9a5e73d59b0bb08853c60c860e7521d08663
Piotr <hynas321@gmail.com>
2022-04-29 17:58:17 +0200

    Initial project commit

---
+++ WinFormsCRUD/Form.Designer.cs
@@ -0,0 +1,402 @@
+﻿
+namespace CRUDXmlFile
+{
+    partial class Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.ID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Governor = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Area = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.MinimumWage = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.TimeZone = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label2 = new System.Windows.Forms.Label();
+            this.IdTextBox = new System.Windows.Forms.TextBox();
+            this.NameTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.GovernorTextBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.AreaTextBox = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.MinimumWageTextBox = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btn_add = new System.Windows.Forms.Button();
+            this.btn_save = new System.Windows.Forms.Button();
+            this.btn_delete = new System.Windows.Forms.Button();
+            this.group_info = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.TimeZoneTextBox = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.ChooseXMLButton = new System.Windows.Forms.Button();
+            this.FileTextBox = new System.Windows.Forms.TextBox();
+            this.btn_update = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.group_info.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ID,
+            this.Name,
+            this.Governor,
+            this.Area,
+            this.MinimumWage,
+            this.TimeZone});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 298);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.Size = new System.Drawing.Size(640, 384);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // ID
+            // 
+            this.ID.DataPropertyName = "id";
+            this.ID.HeaderText = "ID";
+            this.ID.MaxInputLength = 2;
+            this.ID.MinimumWidth = 6;
+            this.ID.Name = "ID";
+            this.ID.Width = 50;
+            // 
+            // Name
+            // 
+            this.Name.DataPropertyName = "name";
+            this.Name.HeaderText = "Name";
+            this.Name.MaxInputLength = 25;
+            this.Name.MinimumWidth = 6;
+            this.Name.Name = "Name";
+            this.Name.Width = 150;
+            // 
+            // Governor
+            // 
+            this.Governor.DataPropertyName = "governor";
+            this.Governor.HeaderText = "Governor";
+            this.Governor.MaxInputLength = 30;
+            this.Governor.MinimumWidth = 6;
+            this.Governor.Name = "Governor";
+            this.Governor.Width = 150;
+            // 
+            // Area
+            // 
+            this.Area.DataPropertyName = "area";
+            this.Area.HeaderText = "Area";
+            this.Area.MaxInputLength = 10;
+            this.Area.MinimumWidth = 6;
+            this.Area.Name = "Area";
+            this.Area.Width = 125;
+            // 
+            // MinimumWage
+            // 
+            this.MinimumWage.DataPropertyName = "minimumwage";
+            this.MinimumWage.HeaderText = "MinimumWage";
+            this.MinimumWage.MaxInputLength = 6;
+            this.MinimumWage.MinimumWidth = 6;
+            this.MinimumWage.Name = "MinimumWage";
+            this.MinimumWage.Width = 125;
+            // 
+            // TimeZone
+            // 
+            this.TimeZone.DataPropertyName = "timezone";
+            this.TimeZone.HeaderText = "TimeZone";
+            this.TimeZone.MaxInputLength = 2;
+            this.TimeZone.MinimumWidth = 6;
+            this.TimeZone.Name = "TimeZone";
+            this.TimeZone.Width = 80;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.label2.Location = new System.Drawing.Point(6, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(23, 19);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "ID";
+            // 
+            // IdTextBox
+            // 
+            this.IdTextBox.Font = new System.Drawing.Font("Segoe UI", 8F);
+            this.IdTextBox.Location = new System.Drawing.Point(114, 41);
+            this.IdTextBox.Name = "IdTextBox";
+            this.IdTextBox.Size = new System.Drawing.Size(44, 22);
+            this.IdTextBox.TabIndex = 3;
+            // 
+            // NameTextBox
+            // 
+            this.NameTextBox.Font = new System.Drawing.Font("Segoe UI", 8F);
+            this.NameTextBox.Location = new System.Drawing.Point(115, 68);
+            this.NameTextBox.Name = "NameTextBox";
+            this.NameTextBox.Size = new System.Drawing.Size(212, 22);
+            this.NameTextBox.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.label3.Location = new System.Drawing.Point(6, 67);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 19);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Name";
+            // 
+            // GovernorTextBox
+            // 
+            this.GovernorTextBox.Font = new System.Drawing.Font("Segoe UI", 8F);
+            this.GovernorTextBox.Location = new System.Drawing.Point(114, 95);
+            this.GovernorTextBox.Name = "GovernorTextBox";
+            this.GovernorTextBox.Size = new System.Drawing.Size(212, 22);
+            this.GovernorTextBox.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.label4.Location = new System.Drawing.Point(6, 94);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(67, 19);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Governor";
+            // 
+            // AreaTextBox
+            // 
+            this.AreaTextBox.Font = new System.Drawing.Font("Segoe UI", 8F);
+            this.AreaTextBox.Location = new System.Drawing.Point(114, 122);
+            this.AreaTextBox.Name = "AreaTextBox";
+            this.AreaTextBox.Size = new System.Drawing.Size(124, 22);
+            this.AreaTextBox.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.label5.Location = new System.Drawing.Point(6, 121);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(37, 19);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Area";
+            // 
+            // MinimumWageTextBox
+            // 
+            this.MinimumWageTextBox.Font = new System.Drawing.Font("Segoe UI", 8F);
+            this.MinimumWageTextBox.Location = new System.Drawing.Point(114, 150);
+            this.MinimumWageTextBox.Name = "MinimumWageTextBox";
+            this.MinimumWageTextBox.Size = new System.Drawing.Size(124, 22);
+            this.MinimumWageTextBox.TabIndex = 11;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.label6.Location = new System.Drawing.Point(6, 150);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(102, 19);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "MinimumWage";
+            // 
+            // btn_add
+            // 
+            this.btn_add.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.btn_add.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.btn_add.Location = new System.Drawing.Point(136, 245);
+            this.btn_add.Name = "btn_add";
+            this.btn_add.Size = new System.Drawing.Size(91, 47);
+            this.btn_add.TabIndex = 12;
+            this.btn_add.Text = "Clear GroupBox";
+            this.btn_add.UseVisualStyleBackColor = false;
+            this.btn_add.Click += new System.EventHandler(this.btn_add_Click);
+            // 
+            // btn_save
+            // 
+            this.btn_save.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.btn_save.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.btn_save.Location = new System.Drawing.Point(14, 245);
+            this.btn_save.Name = "btn_save";
+            this.btn_save.Size = new System.Drawing.Size(99, 47);
+            this.btn_save.TabIndex = 13;
+            this.btn_save.Text = "Add set from GroupBox";
+            this.btn_save.UseVisualStyleBackColor = false;
+            this.btn_save.Click += new System.EventHandler(this.btn_save_Click);
+            // 
+            // btn_delete
+            // 
+            this.btn_delete.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.btn_delete.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.btn_delete.Location = new System.Drawing.Point(362, 245);
+            this.btn_delete.Name = "btn_delete";
+            this.btn_delete.Size = new System.Drawing.Size(91, 47);
+            this.btn_delete.TabIndex = 15;
+            this.btn_delete.Text = "Delete Data Set";
+            this.btn_delete.UseVisualStyleBackColor = false;
+            this.btn_delete.Click += new System.EventHandler(this.btn_delete_Click);
+            // 
+            // group_info
+            // 
+            this.group_info.Controls.Add(this.label1);
+            this.group_info.Controls.Add(this.TimeZoneTextBox);
+            this.group_info.Controls.Add(this.MinimumWageTextBox);
+            this.group_info.Controls.Add(this.label6);
+            this.group_info.Controls.Add(this.AreaTextBox);
+            this.group_info.Controls.Add(this.label5);
+            this.group_info.Controls.Add(this.GovernorTextBox);
+            this.group_info.Controls.Add(this.label4);
+            this.group_info.Controls.Add(this.NameTextBox);
+            this.group_info.Controls.Add(this.label3);
+            this.group_info.Controls.Add(this.IdTextBox);
+            this.group_info.Controls.Add(this.label2);
+            this.group_info.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.group_info.Location = new System.Drawing.Point(12, 22);
+            this.group_info.Name = "group_info";
+            this.group_info.Size = new System.Drawing.Size(353, 204);
+            this.group_info.TabIndex = 16;
+            this.group_info.TabStop = false;
+            this.group_info.Text = "State\'s subelements (GroupBox)";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.label1.Location = new System.Drawing.Point(6, 177);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 19);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "TimeZone";
+            // 
+            // TimeZoneTextBox
+            // 
+            this.TimeZoneTextBox.Font = new System.Drawing.Font("Segoe UI", 8F);
+            this.TimeZoneTextBox.Location = new System.Drawing.Point(114, 177);
+            this.TimeZoneTextBox.Name = "TimeZoneTextBox";
+            this.TimeZoneTextBox.Size = new System.Drawing.Size(44, 22);
+            this.TimeZoneTextBox.TabIndex = 12;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Segoe UI", 25.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label7.Location = new System.Drawing.Point(388, 22);
+            this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(266, 46);
+            this.label7.TabIndex = 17;
+            this.label7.Text = "XML Application";
+            // 
+            // ChooseXMLButton
+            // 
+            this.ChooseXMLButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.ChooseXMLButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F);
+            this.ChooseXMLButton.Location = new System.Drawing.Point(370, 80);
+            this.ChooseXMLButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.ChooseXMLButton.Name = "ChooseXMLButton";
+            this.ChooseXMLButton.Size = new System.Drawing.Size(74, 41);
+            this.ChooseXMLButton.TabIndex = 18;
+            this.ChooseXMLButton.Text = "Choose XML";
+            this.ChooseXMLButton.UseVisualStyleBackColor = false;
+            this.ChooseXMLButton.Click += new System.EventHandler(this.ChooseXMLButton_Click);
+            // 
+            // FileTextBox
+            // 
+            this.FileTextBox.Font = new System.Drawing.Font("Segoe UI", 8F);
+            this.FileTextBox.Location = new System.Drawing.Point(450, 90);
+            this.FileTextBox.Name = "FileTextBox";
+            this.FileTextBox.Size = new System.Drawing.Size(203, 22);
+            this.FileTextBox.TabIndex = 19;
+            // 
+            // btn_update
+            // 
+            this.btn_update.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.btn_update.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.btn_update.Location = new System.Drawing.Point(247, 245);
+            this.btn_update.Name = "btn_update";
+            this.btn_update.Size = new System.Drawing.Size(91, 47);
+            this.btn_update.TabIndex = 20;
+            this.btn_update.Text = "Saves Changes";
+            this.btn_update.UseVisualStyleBackColor = false;
+            this.btn_update.Click += new System.EventHandler(this.btn_update_Click);
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(663, 700);
+            this.Controls.Add(this.btn_update);
+            this.Controls.Add(this.FileTextBox);
+            this.Controls.Add(this.ChooseXMLButton);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.group_info);
+            this.Controls.Add(this.btn_delete);
+            this.Controls.Add(this.btn_save);
+            this.Controls.Add(this.btn_add);
+            this.Controls.Add(this.dataGridView1);
+            this.MaximumSize = new System.Drawing.Size(679, 739);
+            this.MinimumSize = new System.Drawing.Size(679, 739);
+            this.Name = null;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "XML Application";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.group_info.ResumeLayout(false);
+            this.group_info.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox IdTextBox;
+        private System.Windows.Forms.TextBox NameTextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox GovernorTextBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox AreaTextBox;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox MinimumWageTextBox;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btn_add;
+        private System.Windows.Forms.Button btn_save;
+        private System.Windows.Forms.Button btn_delete;
+        private System.Windows.Forms.GroupBox group_info;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox TimeZoneTextBox;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button ChooseXMLButton;
+        private System.Windows.Forms.TextBox FileTextBox;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Name;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Governor;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Area;
+        private System.Windows.Forms.DataGridViewTextBoxColumn MinimumWage;
+        private System.Windows.Forms.DataGridViewTextBoxColumn TimeZone;
+        private System.Windows.Forms.Button btn_update;
+    }
+}
+
---
+++ WinFormsCRUD/Form.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace CRUDXmlFile
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+        private XDocument xmldoc;
+        public Form()
+        {
+            InitializeComponent();
+            btn_add.Enabled = false;
+            btn_delete.Enabled = false;
+            btn_save.Enabled = false;
+            btn_update.Enabled = false;
+            FileTextBox.Enabled = false;
+        }
+
+        private void FileDialog(string filter, string title, TextBox textbox)
+        {
+            var ofd = new OpenFileDialog();
+
+            ofd.Filter = filter;
+            ofd.Title = title;
+            ofd.ShowDialog(this);
+
+            textbox.Text = ofd.FileName;
+        }
+
+        private void ChooseXMLButton_Click(object sender, EventArgs e)
+        {
+            FileDialog("XML Files|*.xml", "Choose an XML file", FileTextBox);
+            try
+            {
+                LoadData();
+                btn_add.Enabled = true;
+                btn_delete.Enabled = true;
+                btn_save.Enabled = true;
+                btn_update.Enabled = true;
+                ChooseXMLButton.Enabled = false;
+                dataGridView1.Enabled = true;
+            }
+            catch { }
+        }
+
+        public void LoadData()
+        {
+            xmldoc = XDocument.Load(FileTextBox.Text);
+            var data = xmldoc.Descendants("state").Select(p => new
+            {
+                id = p.Element("id").Value,
+                name = p.Element("name").Value,
+                governor = p.Element("governor").Value,
+                area = p.Element("area").Value,
+                minimumwage = p.Element("minimumwage").Value,
+                timezone = p.Element("timezone").Value
+            }).OrderBy(p => int.Parse(p.id)).ToList();
+           
+            IdTextBox.DataBindings.Clear();
+            NameTextBox.DataBindings.Clear();
+            GovernorTextBox.DataBindings.Clear();
+            AreaTextBox.DataBindings.Clear();
+            MinimumWageTextBox.DataBindings.Clear();
+            TimeZoneTextBox.DataBindings.Clear();
+
+            IdTextBox.DataBindings.Add("text", data, "id");
+            NameTextBox.DataBindings.Add("text", data, "name");
+            GovernorTextBox.DataBindings.Add("text", data, "governor");
+            AreaTextBox.DataBindings.Add("text", data, "area");
+            MinimumWageTextBox.DataBindings.Add("text", data, "minimumwage");
+            TimeZoneTextBox.DataBindings.Add("text", data, "timezone");
+            dataGridView1.DataSource = data;
+        }
+
+        private void btn_add_Click(object sender, EventArgs e)
+        {
+            foreach (var item in group_info.Controls)
+            {
+                if (item is TextBox)
+                {
+                    (item as TextBox).Clear();
+                }
+            }
+            IdTextBox.Focus();
+        }
+
+        private void btn_save_Click(object sender, EventArgs e)
+        {
+            XElement emp = 
+                new XElement("state",
+                new XElement("id", IdTextBox.Text),
+                new XElement("name", NameTextBox.Text),
+                new XElement("governor", GovernorTextBox.Text),
+                new XElement("area", AreaTextBox.Text),
+                new XElement("minimumwage", MinimumWageTextBox.Text),
+                new XElement("timezone", TimeZoneTextBox.Text));
+            xmldoc.Root.Add(emp);
+            xmldoc.Save(FileTextBox.Text);
+            LoadData();
+            btn_add_Click(null, null);
+        }
+
+        private void btn_update_Click(object sender, EventArgs e)
+        {
+            XElement emp = xmldoc.Descendants("state").FirstOrDefault(p => p.Element("id").Value == IdTextBox.Text);
+            if (emp != null)
+            {
+                emp.Element("name").Value = NameTextBox.Text;
+                emp.Element("governor").Value = GovernorTextBox.Text;
+                emp.Element("area").Value = AreaTextBox.Text;
+                emp.Element("minimumwage").Value = MinimumWageTextBox.Text;
+                emp.Element("timezone").Value = TimeZoneTextBox.Text; ;          
+                xmldoc.Save(FileTextBox.Text);
+                LoadData();
+                btn_add_Click(null, null);
+            }
+        }
+
+        private void btn_delete_Click(object sender, EventArgs e)
+        {
+            XElement emp = xmldoc.Descendants("state").FirstOrDefault(p => p.Element("id").Value == IdTextBox.Text);
+            if (emp != null)
+            {
+                emp.Remove();
+                xmldoc.Save(FileTextBox.Text);
+                LoadData();
+                btn_add_Click(null, null);
+            }
+        }
+    }
+}
